12c69e800f7cd8644483a15bb7b6fdb1b4d84d5d
Edmond Wang <644568491@qq.com>
2019-03-31 21:21:29 +0800

    adjust webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
         }, ],
     },
     output: {
-        filename: 'main.js',
+        filename: 'server/src/main.ts',
         path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
